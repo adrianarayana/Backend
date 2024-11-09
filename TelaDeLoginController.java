92e9dc4f08a377bcaf6b3d60b2cd48542fced375
Adriana Rayana <adrianarayana66@gmail.com>
2024-11-09 11:43:55 -0300

    Add files via upload

---
+++ TelaDeLoginController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import view.*;
+import model.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class TelaDeLoginController extends TelaDeLoginView {
+    public static void logarController() {
+        ArrayList<String> resultados = new ArrayList<String>(TelaDeLoginModel.logarModel(txtLogin.getText(), String.valueOf(txtSenha.getPassword())));
+    }
+
+}
